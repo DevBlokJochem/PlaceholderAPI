381c3f5c03405a1297d3133bdbe978531ec9b5d1
Jochem <jochemminecraftyt@gmail.com>
2022-04-23 09:30:54 +0200

    Added the default template for  the extension

---
+++ build.gradle.kts
@@ -0,0 +1,93 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    // Kotlinx serialization for any data format
+    kotlin("plugin.serialization") version "1.6.10"
+    // Allow publishing
+    `maven-publish`
+
+    // Apply the application plugin to add support for building a jar
+    java
+    // Dokka documentation w/ kotlin
+    id("org.jetbrains.dokka") version "1.6.10"
+}
+
+repositories {
+    // Use mavenCentral
+    mavenCentral()
+
+    maven(url = "https://jitpack.io")
+    maven(url = "https://repo.spongepowered.org/maven")
+    maven(url = "https://repo.minestom.com/repository/maven-public/")
+    maven(url = "https://repo.velocitypowered.com/snapshots/")
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    compileOnly(kotlin("stdlib"))
+
+    // Use the Kotlin reflect library.
+    compileOnly(kotlin("reflect"))
+
+    // Compile Minestom into project
+    compileOnly("com.github.minestom:minestom:708307dbde")
+
+    //json config things
+    implementation("com.google.code.gson:gson:2.8.6")
+
+    //message utils
+    implementation("com.github.Project-Cepi:KStom:e1158a629f")
+    implementation("net.kyori:adventure-api:4.9.3")
+
+    // import kotlinx serialization
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+}
+
+configurations {
+    testImplementation {
+        extendsFrom(configurations.compileOnly.get())
+    }
+}
+
+// Take gradle.properties and apply it to resources.
+tasks {
+    processResources {
+        // Apply properties to extension.json
+        filesMatching("extension.json") {
+            expand(project.properties)
+        }
+    }
+
+    test { useJUnitPlatform() }
+
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.properties["group"] as? String?
+            artifactId = project.name
+            version = project.properties["version"] as? String?
+
+            from(components["java"])
+        }
+    }
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions.jvmTarget = JavaVersion.VERSION_16.toString()
+
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xinline-classes")
+}
---
+++ src/main/kotlin/com/survival/core/EssentialsMain.kt
@@ -0,0 +1,13 @@
+package com.survival.core
+
+import net.minestom.server.extensions.Extension
+
+class Main : Extension() {
+
+    override fun initialize() {
+    }
+
+    override fun terminate() {
+    }
+
+}
